8d3240412f40620704b3cdb58978691dba5cf197
jerry <jerrymic2255@gmail.com>
2021-05-18 22:56:59 -0500

    new code

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import tweepy as t
 import logging
 import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger()
 
-note = open('data.txt','w')
-
 def auth_cred(): #authentication process
-    api_token = "T5bpMN8s9Ssha1JyWiwUXicyZ"
-    api_secret = "eeO7QNujWuugP19nkyx9KaB21oycEacAPU2837yudLqJxXsFWw"
-    auth_token = "1393746703270907904-IYUtggPcpcG9nBl5rZhumkFaxY10Xv"
-    auth_secret_token = "Xy6CqYDe1nM09y0K2KRf6cOwobkYmyEjLY87wSuKUlQGs"
+    api_token = os.getenv('API_KEY')
+    api_secret = os.getenv('API_SECRET')
+    auth_token = os.getenv('ACCESS_KEY')
+    auth_secret_token = os.getenv('ACCESS_SECRET')
 
     auth = t.OAuthHandler(api_token , api_secret) #using oauth authetication is done
     auth.set_access_token(auth_token , auth_secret_token)
 
     global api # referred as global for it being used outside the function
-    api = t.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True) #api is declared
+    api = t.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True) #api is been declared
 
 
     try:  #verifying the credentials
@@ -28,15 +29,20 @@ def auth_cred(): #authentication process
     return api
 
 
-def since_id(): #since_id inorder to fetch the id os the last tweet(feature)
-    tweety = []
-    for tweet in t.Cursor(api.mentions_timeline).items():
-        if len(tweety) > 1:
-            break
-        else:
-            tweety.append(tweet)
-            new_tweet = tweety[0].id
-        return new_tweet
+# def since_id(): #since_id inorder to fetch the id of the last tweet(feature)
+#     tweety = []
+#     for tweet in t.Cursor(api.mentions_timeline).items():
+#         if len(tweety) > 1:
+#             break
+#         else:
+#             tweety.append(tweet)
+#             new_tweet = tweety[0].id
+#         return new_tweet
+
+def data_txt(obj):
+    file = open('data.txt','w')
+    file.write(obj)
+    file.close()
 
 def Mention(api,since_id): #mentioning function
 
@@ -51,18 +57,15 @@ def Mention(api,since_id): #mentioning function
             status_id = tweet.in_reply_to_status_id
             tweet_re = api.get_status(status_id,tweet_mode='extended')
             print(tweet_re.full_text)
-
-
     return new_since_id
 
 def main():
     api = auth_cred()
-    id = since_id()  #the last mention you have.
+    id = 1 #the last mention you have.
     while True:
         print(id)
         mention = Mention(api,id)
-        print(mention)
-        note.write(mention)
+        data_txt(mention)
         logger.info("Waiting...")
         time.sleep(60)
 
